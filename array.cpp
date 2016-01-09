6ec3f3e6dbf407c3b556d0041e9bb57a4168b89e
samchen <git@samchen.com>
2016-01-09 15:07:07 +0800

    OK
    OK

--- array.cpp
+++ array.cpp
@@ -8,8 +8,27 @@
 
 #include <stdio.h>
 #include <iostream>
+
+
+
 using namespace std;
 
+
 //int main(){
-//    cout<<"hello"<<endl;
+////    const unsigned short ITEM =10;
+//    int num[10];
+//    cout<<"Please input 10 nums\n\n";
+//    for (int i=0; i<10; i++) {
+//        cout<<"Please input num "<<i+1<<" \n";
+//        cin>>num[i];
+//    }
+//    int total =0;
+//    for(int i=0;i<10;i++){
+//        total+=num[i];
+//    }
+//    
+//    cout<<"cout is "<<total<<endl;
+//    cout<<"avg is "<<(float)total/10<<endl;
+//    
+//    return 0;
 //}
---
+++ example_type.cpp
@@ -0,0 +1,18 @@
+//
+//  example_type.cpp
+//  Work
+//
+//  Created by Samchen on 1/9/16.
+//  Copyright (c) 2016 Samchen. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//int main(){
+//    string str;
+//    getline(cin, str);
+//    cout<<str<<endl;
+//    return 0;
+//}
